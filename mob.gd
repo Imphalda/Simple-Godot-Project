110b050b4e182473d0ff38cd07495a6845610484
jamofthetoe <toejam.urich2@gmail.com>
2024-10-05 18:37:02 -0500

    Mob functionality

--- mob.gd
+++ mob.gd
@@ -3,7 +3,8 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var mob_types = $animation_Mob.sprite_frames.get_animation_names()
+	$animation_Mob.play(mob_types[1])
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.

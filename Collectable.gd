7112fefd8e6915c754bef691c962a4e8f07927c9
Imphalda <fortdeath9@gmail.com>
2024-10-05 21:31:19 -0500

    Whoops

--- Collectable.gd
+++ Collectable.gd
@@ -8,14 +8,11 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	
 	pass
 
-#God please I swear
-
-func _on_body_entered(body: Node) -> void:
-	if body.find_parent("Player").name == "Player":
-		collected.emit()
-		hide()
-		$CollisionObject2D.set_deferred("disabled", true)
+func _on_body_shape_entered(body_rid: RID, body: Node, body_shape_index: int, local_shape_index: int) -> void:
+	if body.is_in_group("Player"):
+	collected.emit()
+	hide()
+	$CollisionObject2D.set_deferred("disabled", true)
 	pass # Replace with function body.

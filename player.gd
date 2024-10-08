8a3c158d5a67dfa0fa5a90641682ee6d5959b579
jamofthetoe <toejam.urich2@gmail.com>
2024-10-08 14:47:22 -0500

    First attempt at player gravity and making collision and texture inherited

--- player.gd
+++ player.gd
@@ -1,33 +1,53 @@
 extends Area2D
 signal hit
-@export var speed = 400
+@export var speed = 300
 var screen_size
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
 	hide()
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var velocity = Vector2.ZERO
+	
+	#Gravity
+	velocity += Vector2.DOWN * speed * delta
+	
+	"""
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
-	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
+	"""
 	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
-
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		$AnimatedSprite2D.play()
+		velocity = Vector2.UP * 5.0
+		$AnimatedSprite2D.animation = "up"
+	
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= speed
+		if !velocity.y:
+			$AnimatedSprite2D.animation = "walk"
+	
+	if Input.is_action_pressed("move_right"):
+		velocity.x += speed
+		if !velocity.y:
+			$AnimatedSprite2D.animation = "walk"
+	
+	"""
+	if velocity.x != 0 and velocity.y != 0:
+		velocity
+	if velocity.x != 0:
+		velocity = velocity * speed
+		$AnimatedSprite2D.play(player_types[1])
 	else:
 		$AnimatedSprite2D.stop()
+	"""
 
-	position += velocity * delta
+	position += velocity * speed * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
 	
 	if velocity.x != 0:
